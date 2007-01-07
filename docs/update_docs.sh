589de5a06228fdfff74d4949f40cee94cfa0ce2e
Graeme Geldenhuys <graemeg@users.sourceforge.net>
2007-01-07 10:30:47 +0000

    * Update the fpdoc scripts. * Reorganized the docs directory to match the source directories.

--- docs/update_docs.sh
+++ docs/update_docs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # fpc/bin must be in your PATH
 makeskel --update --package=fpgui \
-  --input='-Fi../src ../src/fpgui.pp' --descr="xml/fpgui.xml" --output="xml/fpgui.new.xml"
+  --input='-Fi../gui ../gui/fpgui.pas' --descr="xml/gui/fpgui.xml" --output="xml/gui/fpgui.new.xml" \
+  --input='-Fi../gui ../gui/stylemanager.pas' --descr="xml/gui/stylemanager.xml" --output="xml/gui/stylemanager.new.xml"
