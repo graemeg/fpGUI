1fa3f8361b9793541b583c913d67691b0502b6af
Graeme Geldenhuys <graemeg@users.sourceforge.net>
2007-01-07 11:12:46 +0000

    more docs changes

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # fpc/bin must be in your PATH
-fpdoc --package=fpgui --format=html --output=html/  \
+fpdoc --package=fpgui --format=html --output=html/gui/  \
   --descr=xml/gui/fpgui.xml --input='-Fi../gui ../gui/fpgui.pas' \
   --descr=xml/gui/stylemanager.xml --input='-Fi../gui ../gui/stylemanager.pas'
 
