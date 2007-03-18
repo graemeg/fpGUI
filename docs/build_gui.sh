38020d5baa685bb03910b02f057c2179db149723
Graeme Geldenhuys <graemeg@users.sourceforge.net>
2007-03-18 17:15:46 +0000

    more documentation updates as well as scrip updates to build the documentation.

--- docs/build_gui.sh
+++ docs/build_gui.sh
@@ -6,5 +6,8 @@ fpdoc --package=fpgui \
   --content=html/gui.cnt \
   --import=html/gfx.cnt,../gfx/ \
   --descr=xml/gui/fpgui.xml --input='-Fi../gui ../gui/fpgui.pas' \
-  --descr=xml/gui/stylemanager.xml --input='-Fi../gui ../gui/stylemanager.pas'
+  --descr=xml/gui/stylemanager.xml --input='-Fi../gui ../gui/stylemanager.pas' \
+  --descr=xml/gui/windowsstyle.xml --input='-Fi../gui ../gui/windowsstyle.pas' \
+  --descr=xml/gui/motifstyle.xml --input='-Fi../gui ../gui/motifstyle.pas' \
+  --descr=xml/gui/opensoftstyle.xml --input='-Fi../gui ../gui/opensoftstyle.pas' 
 
---
+++ docs/xml/gui/examples/TStyleManager.RegisterClass.pas
@@ -0,0 +1,6 @@
+initialization
+  gStyleManager.RegisterClass(cDefaultStyle, TWindowsStyle);
+  gStyleManager.RegisterClass('Windows', TWindowsStyle);
+  gStyleManager.RegisterClass('OpenSoft', TOpenSoftStyle);
+  gStyleManager.RegisterClass('Motif', TMotifStyle);  
+
