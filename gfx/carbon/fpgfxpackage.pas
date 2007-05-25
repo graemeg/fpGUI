2e7b69b9ee7d94c2f3bb9ec7d981e0e5a878fba4
sekelsenmat <sekelsenmat@ae50a9b5-8222-0410-bf8d-8a13f76226bf>
2007-05-25 22:16:22 +0000

    Added initial carbon interface

---
+++ gfx/carbon/fpgfxpackage.pas
@@ -0,0 +1,15 @@
+{ This file was automatically created by Lazarus. Do not edit!
+This source is only used to compile and install the package.
+ }
+
+unit fpgfxpackage; 
+
+interface
+
+uses
+  GfxBase, GFX_X11, gfxinterface, schar16, unitkeys, fpgfx, GELDirty, GELImage, 
+    CommandLineParams, fpUTF8Utils; 
+
+implementation
+
+end.
