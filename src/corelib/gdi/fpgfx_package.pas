6dccfb0375b69e2661174d0728165980b71706c2
graemeg <graemeg@ae50a9b5-8222-0410-bf8d-8a13f76226bf>
2008-02-13 19:15:25 +0000

    * Created a new ImageList unit and class.

--- src/corelib/gdi/fpgfx_package.pas
+++ src/corelib/gdi/fpgfx_package.pas
@@ -8,8 +8,9 @@ interface
 
 uses
   gfxbase, fpgfx, gfx_gdi, gfx_stdimages, gfx_imgfmt_bmp, gfx_widget, 
-    gfx_UTF8utils, gfx_extinterpolation, gfx_cmdlineparams, gfx_clipboard, 
-    gfx_utils, gfx_popupwindow, gfx_impl, gfx_command_intf, gfx_wuline; 
+  gfx_UTF8utils, gfx_extinterpolation, gfx_cmdlineparams, gfx_clipboard, 
+  gfx_utils, gfx_popupwindow, gfx_impl, gfx_command_intf, gfx_wuline, 
+  gfx_imagelist; 
 
 implementation
 
