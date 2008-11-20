57452e6a3aaa0069afe470405965f86ca90cc1ef
graemeg <graemeg@ae50a9b5-8222-0410-bf8d-8a13f76226bf>
2008-11-20 11:00:48 +0000

    * minor adjustment to tiGUIIni

--- extras/tiopf/gui/tiGUIINI.pas
+++ extras/tiopf/gui/tiGUIINI.pas
@@ -67,20 +67,26 @@ begin
     else
       AForm.WindowPosition := wpScreenCenter;
   end;
-  // Only set the form size if a bsSizable window
-  if AForm.Sizeable then
-  begin
-    if AHeight = -1 then
-      LHeight := AForm.Height
-    else
-      LHeight := AHeight;
-    if AWidth = -1 then
-      LWidth := AForm.Width
-    else
-      LWidth := AWidth;
-    AForm.Height  := readInteger(LINISection, 'Height', LHeight);
-    AForm.Width   := readInteger(LINISection, 'Width',  LWidth);
-  end;
+
+  { 2008-11-20 graemeg: disabled Width and Height settings for now. It causes
+    major headaches, plus in most applications the forms have been designed
+    with specific sizes for best look and fit. The user can still adjust sizes
+    at runtime, they will just not be remembered. I will attend to this issue
+    at a later date. }
+    // Only set the form size if a bsSizable window
+  //if AForm.Sizeable then
+  //begin
+    //if AHeight = -1 then
+      //LHeight := AForm.Height
+    //else
+      //LHeight := AHeight;
+    //if AWidth = -1 then
+      //LWidth := AForm.Width
+    //else
+      //LWidth := AWidth;
+    //AForm.Height  := readInteger(LINISection, 'Height', LHeight);
+    //AForm.Width   := readInteger(LINISection, 'Width',  LWidth);
+  //end;
 
   // If the form is off screen (positioned outside all monitor screens) then
   // center the form on screen.
