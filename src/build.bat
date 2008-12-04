ebce21e3bc782bcc54cf0456a043a46e928197c8
graemeg <graemeg@ae50a9b5-8222-0410-bf8d-8a13f76226bf>
2008-12-04 07:11:47 +0000

    * updated windows build batch file to create output lib dir.

--- src/build.bat
+++ src/build.bat
@@ -1,2 +1,14 @@
+@echo off
+; I don't know how to store the output to a variable:  fpctarget=`fpc -iTP`-`fpc -iTO`
+if exist ..\lib\i386-win32\nul.x goto exists
+echo "Creating missing directory ..\lib\i386-win32"
+mkdir ..\lib\i386-win32
+goto end
+
+:exists
+echo "You've got the correct output lib directory"
+
+:end
+
 fpc -dRELEASE -dGDI @extrafpc.cfg corelib\gdi\fpgui_toolkit.pas
 
