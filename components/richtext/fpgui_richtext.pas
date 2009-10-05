ae274a4eefaffa366b6644b757932123f9654fa7
Graeme Geldenhuys <graeme@mastermaths.co.za>
2009-10-05 14:57:12 +0200

    RichText component. Initial checking.
    * RichTextView will become a edit component used in
      fpGUI Help Viewer and probably in fpGUI Reporting.
    
    Signed-off-by: Graeme Geldenhuys <graeme@mastermaths.co.za>

---
+++ components/richtext/fpgui_richtext.pas
@@ -0,0 +1,20 @@
+{ This file was automatically created by Lazarus. do not edit ! 
+  This source is only used to compile and install the package.
+ }
+
+unit fpgui_richtext; 
+
+interface
+
+uses
+  RichTextDocumentUnit, LazarusPackageIntf;
+
+implementation
+
+procedure Register; 
+begin
+end; 
+
+initialization
+  RegisterPackage('fpgui_richtext', @Register); 
+end.
