8b9e0ad51c9b8503cf9a989e8bd6d1defee50728
Graeme Geldenhuys <graeme@mastermaths.co.za>
2010-08-16 11:49:30 +0200

    Updated project templates for use with MSEide.
    * Also included a new "Dark Pascal" color scheme

---
+++ extras/mseide_templates/fpgui_single_unit/project.pas
@@ -0,0 +1,55 @@
+program ${%PROJECTNAME%};
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Classes, fpg_main, fpg_form;
+
+type
+
+  TMainForm = class(TfpgForm)
+  public
+    {@VFD_HEAD_BEGIN: MainForm}
+    {@VFD_HEAD_END: MainForm}
+    procedure AfterCreate; override;
+  end;
+
+{@VFD_NEWFORM_DECL}
+
+
+
+{@VFD_NEWFORM_IMPL}
+
+procedure TMainForm.AfterCreate;
+begin
+  {@VFD_BODY_BEGIN: MainForm}
+  Name := 'MainForm';
+  SetPosition(316, 186, 300, 250);
+  WindowTitle := 'MainForm';
+
+  {@VFD_BODY_END: MainForm}
+end;
+
+
+procedure MainProc;
+var
+  frm: TMainForm;
+begin
+  fpgApplication.Initialize;
+  frm := TMainForm.Create(nil);
+  try
+    frm.Show;
+    fpgApplication.Run;
+  finally
+    frm.Free;
+  end;
+end;
+
+begin
+  MainProc;
+end.
+
+
