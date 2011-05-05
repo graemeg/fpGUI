9b4e628b94cf12ef45a0cf634d85849d4d04b591
Graeme Geldenhuys <graeme@mastermaths.co.za>
2011-05-05 23:59:41 +0200

    Added empty placeholder unit back into src directory.
    This unit will later contain the TfpgCommonStyle (currently known
    as TfpgStyle) code.

---
+++ src/gui/fpg_style.pas
@@ -0,0 +1,30 @@
+{
+    fpGUI  -  Free Pascal GUI Toolkit
+
+    Copyright (C) 2006 - 2011 See the file AUTHORS.txt, included in this
+    distribution, for details of the copyright.
+
+    See the file COPYING.modifiedLGPL, included in this distribution,
+    for details about redistributing fpGUI.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    Description:
+      This is where all style related types should be defined. The base/common
+      style class should also be moved here.
+}
+
+{ TODO: Move the TfpgStyle class here and name it TfpgCommonStyle }
+
+unit fpg_style;
+
+{$mode objfpc}{$H+}
+
+interface
+
+implementation
+
+end.
+
