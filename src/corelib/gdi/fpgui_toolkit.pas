495829859e3e8c5f1827c331ce237fecf813eb8b
Graeme Geldenhuys <graeme@mastermaths.co.za>
2012-08-01 23:41:28 +0100

    Updates te fpgui_toolkit.lpk (lazarus packages) with the reporting engine units.
    * I also updated the windows version of the package with some missing units.

--- src/corelib/gdi/fpgui_toolkit.pas
+++ src/corelib/gdi/fpgui_toolkit.pas
@@ -10,18 +10,19 @@ uses
     fpg_base, fpg_main, fpg_cmdlineparams, fpg_command_intf, fpg_constants, 
   fpg_extinterpolation, fpg_imagelist, fpg_imgfmt_bmp, fpg_pofiles, 
   fpg_popupwindow, fpg_stdimages, fpg_stringhashlist, fpg_translations, 
-  fpg_stringutils, fpg_utils, fpg_widget, fpg_wuline, fpg_animation, 
-  fpg_basegrid, fpg_button, fpg_checkbox, fpg_combobox, fpg_customgrid, 
-  fpg_dialogs, fpg_editcombo, fpg_edit, fpg_form, fpg_gauge, fpg_grid, 
-  fpg_hyperlink, fpg_iniutils, fpg_label, fpg_listbox, fpg_listview, fpg_memo, 
-  fpg_menu, fpg_mru, fpg_panel, fpg_popupcalendar, fpg_progressbar, 
-  fpg_radiobutton, fpg_scrollbar, fpg_style, fpg_tab, fpg_trackbar, fpg_tree, 
-  fpgui_db, fpg_gdi, fpg_impl, fpg_splitter, fpg_hint, fpg_spinedit, 
-  fpg_extgraphics, fpg_ColorMapping, fpg_ColorWheel, fpg_interface, 
-  fpg_editbtn, fpg_imgfmt_jpg, fpg_imgutils, fpg_OLEDragDrop, 
-  fpg_stylemanager, fpg_style_win2k, fpg_style_motif, fpg_style_clearlooks, 
-  fpg_style_bluecurve, fpg_style_bitmap, fpg_readonly, fpg_imgfmt_png;
+  fpg_stringutils, fpg_utils, fpg_widget, fpg_wuline, fpg_impl, fpg_gdi, 
+  fpg_OLEDragDrop, fpg_animation, fpg_basegrid, fpg_button, fpg_checkbox, 
+  fpg_combobox, fpg_customgrid, fpg_dialogs, fpg_editcombo, fpg_edit, 
+  fpg_form, fpg_gauge, fpg_grid, fpg_hyperlink, fpg_iniutils, fpg_label, 
+  fpg_listbox, fpg_listview, fpg_memo, fpg_menu, fpg_mru, fpg_panel, 
+  fpg_popupcalendar, fpg_progressbar, fpg_radiobutton, fpg_scrollbar, 
+  fpg_style, fpg_tab, fpg_trackbar, fpg_tree, fpgui_db, fpg_splitter, 
+  fpg_hint, fpg_spinedit, fpg_extgraphics, fpg_ColorMapping, fpg_ColorWheel, 
+  fpg_interface, fpg_editbtn, fpg_imgfmt_jpg, fpg_imgutils, fpg_stylemanager, 
+  fpg_style_win2k, fpg_style_motif, fpg_style_clearlooks, fpg_style_bluecurve, 
+  fpg_style_bitmap, fpg_readonly, fpg_imgfmt_png, U_Command, U_Pdf, U_Report, 
+  U_ReportImages, U_Visu;
 
 implementation
 
-end.
+end.
--- src/corelib/x11/fpgui_toolkit.pas
+++ src/corelib/x11/fpgui_toolkit.pas
@@ -2,12 +2,12 @@
   This source is only used to compile and install the package.
  }
 
-unit fpgui_toolkit; 
+unit fpgui_toolkit;
 
 interface
 
 uses
-    fpg_base, fpg_main, fpg_cmdlineparams, fpg_command_intf, fpg_constants, 
+  fpg_base, fpg_main, fpg_cmdlineparams, fpg_command_intf, fpg_constants, 
   fpg_extinterpolation, fpg_imagelist, fpg_imgfmt_bmp, fpg_pofiles, fpg_popupwindow, 
   fpg_stdimages, fpg_stringhashlist, fpg_translations, fpg_stringutils, fpg_utils, 
   fpg_widget, fpg_wuline, fpg_impl, fpg_x11, fpg_netlayer_x11, fpg_keyconv_x11, 
@@ -19,7 +19,8 @@ uses
   fpg_hint, fpg_spinedit, fpg_extgraphics, fpg_ColorMapping, fpg_ColorWheel, 
   fpg_interface, fpg_editbtn, fpg_imgfmt_jpg, fpg_imgutils, fpg_stylemanager, 
   fpg_style_win2k, fpg_style_motif, fpg_style_clearlooks, fpg_style_bluecurve, 
-  fpg_style_bitmap, fpg_readonly, fpg_imgfmt_png;
+  fpg_style_bitmap, fpg_readonly, fpg_imgfmt_png, U_Command, U_Pdf, U_Report, 
+  U_ReportImages, U_Visu;
 
 implementation
 
