5fce370be96f9ff24a908f301092429bb88b4f76
Graeme Geldenhuys <graemeg@gmail.com>
2013-05-13 12:08:24 +0100

    docview: replace some code with convenience function to make it more readable

--- docview/components/richtext/RichTextView.pas
+++ docview/components/richtext/RichTextView.pas
@@ -1007,7 +1007,7 @@ begin
   if InDesigner then
     exit;
 
-  if WinHandle = 0 then
+  if IsLoading then
     exit;
 
   RemoveCursor;
@@ -1066,8 +1066,10 @@ ProfileEvent('DEBUG:  TRichTextView.Layout >>>>');
 
   if InDesigner then
     exit;
-  if WinHandle = 0 then
+
+  if IsLoading then
     exit;
+
 ProfileEvent('DEBUG:  TRichTextView.Layout    1 of 6');
   FSelectionEnd := -1;
   FSelectionStart := -1;
