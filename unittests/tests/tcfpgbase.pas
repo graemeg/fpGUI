e92286982c2dda7fdd958c5fce94baf4c956a228
Graeme Geldenhuys <graemeg@gmail.com>
2016-11-26 12:05:50 +0000

    units tests: more TfpgRect unit tests added.

--- unittests/tests/tcfpgbase.pas
+++ unittests/tests/tcfpgbase.pas
@@ -17,8 +17,12 @@ type
     procedure CheckEqualsRect(const expected, actual: TfpgRect; const ErrorMsg: string= '');
   published
     procedure TestSetRect;
+    procedure TestClear;
+    procedure TestIsUnassigned;
+    procedure TestIsRectEmpty;
     procedure TestBottom;
     procedure TestRight;
+    procedure TestSetBottom;
     procedure TestUnionRect_Old_vs_New;
   end;
 
@@ -61,6 +65,69 @@ begin
   CheckEquals(40, r1.Height, 'Failed on 4');
 end;
 
+procedure TTestFPGRect.TestClear;
+var
+  r1: TfpgRect;
+begin
+  r1.SetRect(10, 20, 30, 40);
+  r1.Clear;
+  CheckEquals(0, r1.Left, 'Failed on 1');
+  CheckEquals(0, r1.Top, 'Failed on 2');
+  CheckEquals(0, r1.Width, 'Failed on 3');
+  CheckEquals(0, r1.Height, 'Failed on 4');
+end;
+
+procedure TTestFPGRect.TestIsUnassigned;
+var
+  r1: TfpgRect;
+begin
+  r1.SetRect(10, 20, 30, 40);
+  r1.Clear;
+  CheckEquals(0, r1.Left, 'Failed on 1');
+  CheckEquals(0, r1.Top, 'Failed on 2');
+  CheckEquals(0, r1.Width, 'Failed on 3');
+  CheckEquals(0, r1.Height, 'Failed on 4');
+  CheckEquals(true, r1.IsUnassigned, 'Failed on 5');
+  r1.Clear;
+  r1.Left := 1;
+  CheckEquals(false, r1.IsUnassigned, 'Failed on 6');
+  r1.Clear;
+  r1.Top := 1;
+  CheckEquals(false, r1.IsUnassigned, 'Failed on 7');
+  r1.Clear;
+  r1.Width := 1;
+  CheckEquals(false, r1.IsUnassigned, 'Failed on 8');
+  r1.Clear;
+  r1.Height := 1;
+  CheckEquals(false, r1.IsUnassigned, 'Failed on 9');
+end;
+
+procedure TTestFPGRect.TestIsRectEmpty;
+var
+  r1: TfpgRect;
+begin
+  r1.Clear;
+  CheckEquals(true, r1.IsRectEmpty, 'Failed on 1');
+  r1.Clear;
+  r1.Left := 1;
+  CheckEquals(true, r1.IsRectEmpty, 'Failed on 2');
+  r1.Clear;
+  r1.Top := 1;
+  CheckEquals(true, r1.IsRectEmpty, 'Failed on 3');
+  r1.Clear;
+  r1.Width := 1;
+  // a rect with no height is not a rectangle
+  CheckEquals(true, r1.IsRectEmpty, 'Failed on 4');
+  r1.Clear;
+  r1.Height := 1;
+  // a rect with no width is not a rectangle
+  CheckEquals(true, r1.IsRectEmpty, 'Failed on 5');
+  r1.Clear;
+  r1.Width := 1;
+  r1.Height := 1;
+  CheckEquals(false, r1.IsRectEmpty, 'Failed on 6');
+end;
+
 { See the TfpgRect API documentation for more details, if you don't understand. }
 procedure TTestFPGRect.TestBottom;
 var
@@ -87,6 +154,16 @@ begin
   CheckEquals(1, r1.Right, 'Failed on 3');
 end;
 
+procedure TTestFPGRect.TestSetBottom;
+var
+  r1: TfpgRect;
+begin
+  r1.SetRect(0, 0, 0, 0);
+  r1.SetBottom(5);
+  CheckEquals(5, r1.Bottom, 'Failed on 1');
+  CheckEquals(6, r1.Height, 'Failed on 2');
+end;
+
 procedure TTestFPGRect.TestUnionRect_Old_vs_New;
 var
   r1, r2: TfpgRect;
@@ -102,5 +179,6 @@ begin
   r1.UnionRect(lResult, r2);
   CheckEqualsRect(lRef, lResult, 'Failed on 2');
 end;
+
 
 end.
