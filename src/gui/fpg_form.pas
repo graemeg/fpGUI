15334242042571f8c478568078bbbf63f035821a
Graeme Geldenhuys <graemeg@gmail.com>
2017-05-21 23:51:30 +0100

    DPI scaling implemented for any TfpgForm descendants.

--- src/gui/fpg_form.pas
+++ src/gui/fpg_form.pas
@@ -1,7 +1,7 @@
 {
     This unit is part of the fpGUI Toolkit project.
 
-    Copyright (c) 2006 - 2015 by Graeme Geldenhuys.
+    Copyright (c) 2006 - 2017 by Graeme Geldenhuys.
 
     See the file COPYING.modifiedLGPL, included in this distribution,
     for details about redistributing fpGUI.
@@ -33,7 +33,7 @@ uses
 type
   TWindowPosition = (wpUser, wpAuto, wpScreenCenter, wpOneThirdDown);
   TCloseAction = (caNone, caHide, caFree{, caMinimize});
-  
+
   TFormCloseEvent = procedure(Sender: TObject; var CloseAction: TCloseAction) of object;
   TFormCloseQueryEvent = procedure(Sender: TObject; var CanClose: boolean) of object;
   TfpgHelpEvent = function(AHelpType: THelpType; AHelpContext: THelpContext;
@@ -99,14 +99,15 @@ type
     procedure   AdjustWindowStyle; override;
     procedure   SetWindowParameters; override;
     procedure   InvokeHelp; override;
+    procedure   ScaleDPI(AFromDPI: integer = 0);
     procedure   Show;
     procedure   Hide;
     function    ShowModal: TfpgModalResult;
     procedure   Close;
     function    CloseQuery: boolean; virtual;
   end;
-  
-  
+
+
   TfpgForm = class(TfpgBaseForm)
   published
     property    BackgroundColor;
@@ -161,16 +162,14 @@ implementation
 uses
   fpg_main,
   fpg_popupwindow,
-  fpg_menu;
-  
+  fpg_menu,
+  fpg_basegrid;
+
 type
   // to access protected methods
-  TfpgMenuBarFriend = class(TfpgMenuBar)
-  end;
-
-  TfpgWidgetFriend = class(TfpgWidget)
-  end;
-
+  TfpgMenuBarFriend = class(TfpgMenuBar);
+  TfpgWidgetFriend = class(TfpgWidget);
+  TfpgGridFriend = class(TfpgBaseGrid);
 
 function WidgetParentForm(wg: TfpgWidget): TfpgForm;
 var
@@ -187,6 +186,77 @@ begin
     w := w.Parent;
   end;
   Result := nil;
+end;
+
+function MathRound(AValue: ValReal): Int64; inline;
+begin
+  if AValue >= 0 then
+    Result := Trunc(AValue + 0.5)
+  else
+    Result := Trunc(AValue - 0.5);
+end;
+
+function MulDiv(nNumber, nNumerator, nDenominator: Integer): Integer;
+begin
+  if nDenominator = 0 then
+    Result := -1
+  else
+    Result := MathRound(int64(nNumber) * int64(nNumerator) / nDenominator);
+end;
+
+function ScaleX(const SizeX, FromDPI: Integer): Integer;
+begin
+  Result := MulDiv(SizeX, fpgApplication.Screen_dpi_x, FromDPI);
+end;
+
+function ScaleY(const SizeY, FromDPI: Integer): Integer;
+begin
+  Result := MulDiv(SizeY, fpgApplication.Screen_dpi_y, FromDPI);
+end;
+
+procedure uScaleDPI(Control: TComponent; FromDPI: Integer);
+var
+  n: integer;
+  c: integer;
+  widget: TfpgWidget;
+begin
+  if Control is TfpgWidget then
+  begin
+    widget := TfpgWidget(Control);
+    TfpgWidgetFriend(widget).Loading;
+    with widget do
+    begin
+      Left := ScaleX(Left,FromDPI);
+      Top := ScaleY(Top,FromDPI);
+      Width := ScaleX(Width,FromDPI);
+      Height := ScaleY(Height,FromDPI);
+
+      MinWidth := ScaleX(MinWidth, FromDPI);
+      MinHeight := ScaleY(MinHeight, FromDPI);
+      MaxWidth := ScaleX(MaxWidth, FromDPI);
+      MaxHeight := ScaleY(MaxHeight, FromDPI);
+
+      // grids get special treatment
+      if (widget is TfpgBaseGrid) then
+      begin
+        for c := 0 to TfpgGridFriend(widget).ColumnCount-1 do
+          TfpgGridFriend(widget).ColumnWidth[c] := ScaleX(TfpgGridFriend(widget).ColumnWidth[c], FromDPI);
+      end;
+    end;
+    TfpgWidgetFriend(widget).Loaded;
+    // process children
+    if widget.ComponentCount > 0 then
+    begin
+      for n := 0 to widget.ComponentCount-1 do
+      begin
+        if widget.Components[n] is TfpgWidget then
+        begin
+          uScaleDPI(widget.Components[n], FromDPI);
+        end;
+      end;
+    end;
+    widget.UpdatePosition;
+  end; { if Control }
 end;
 
 { TfpgBaseForm }
@@ -208,7 +278,7 @@ begin
     DebugLn('Inside if block');
     {$ENDIF}
     FocusRootWidget := self;
-    
+
     if FFormDesigner <> nil then
     begin
       FFormDesigner.Dispatch(msg);
@@ -270,7 +340,7 @@ begin
     Include(WindowAttributes, waSizeable)
   else
     Exclude(WindowAttributes, waSizeable);
-    
+
   if FFullScreen then
     Include(WindowAttributes, waFullScreen)
   else
@@ -324,6 +394,17 @@ begin
     inherited InvokeHelp;
 end;
 
+procedure TfpgBaseForm.ScaleDPI(AFromDPI: integer);
+begin
+  // should we default to the global Applicatien DPI value
+  if AFromDPI = 0 then
+    AFromDPI := fpgApplication.DesignedDPI;
+  // do we actually need to do any scaling?
+  if AFromDPI = fpgApplication.Screen_dpi then
+    Exit; //==>
+  uScaleDPI(self, AFromDPI);
+end;
+
 procedure TfpgBaseForm.Show;
 {$IFDEF CStackDebug}
 var
@@ -369,7 +450,7 @@ begin
     fpgApplication.PopModalForm;
     Result := ModalResult;
   end;  { try..finally }
-  
+
   if ModalResult <> mrNone then
   begin
     lCloseAction := caHide; // Dummy variable - we do nothing with it
@@ -397,7 +478,9 @@ end;
 procedure TfpgBaseForm.HandleShow;
 begin
   inherited HandleShow;
+  ScaleDPI;
   HandleAlignments(0, 0);
+
   if Assigned(FOnShow) then
     FOnShow(self);
 end;
