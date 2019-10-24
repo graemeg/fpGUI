980fc6087ea9365c687bbde394b858f47bae87a8
Graeme Geldenhuys <graemeg@gmail.com>
2019-10-24 22:26:01 +0100

    Tweaked the build.sh script to support multiple platforms

--- src/build.sh
+++ src/build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+echo "Which platform are you building for?"
+echo " "
+echo "  L - Linux"
+echo "  F - FreeBSD"
+echo "  M - Mac OSX"
+echo " "
+
+read -p "Enter a letter or Ctrl+C to quit: " OSinput
+
+
+
 fpcbin=fpc
 fpctarget=`$fpcbin -iTP`-`$fpcbin -iTO`
 #echo $fpctarget
@@ -12,8 +23,27 @@ if [ ! -d $libpath ]; then
   echo ' '
 fi
 
+
+case $OSinput in
+	"L"|"l")
+		echo "Compiling for Linux"
+		$fpcbin -dX11 @extrafpc.cfg corelib/x11/fpgui_toolkit.pas
+		;;
+	"F"|"f")
+		echo "Compiling for FreeBSD"
+		$fpcbin -dX11 @extrafpc.cfg corelib/x11/fpgui_toolkit.pas
+		;;
+	"M"|"m")
+		echo "Compiling for OSX Cocoa"
+		$fpcbin -dCocoa @extrafpc.cfg corelib/cocoa/fpgui_toolkit.pas
+		;;
+	*)
+		echo "Unknown option - doing nothing!"
+		;;
+esac
+
 # LINUX & FREEBSD
 #$fpcbin -dX11 @extrafpc.cfg corelib/x11/fpgui_toolkit.pas
 
 # OSX
-$fpcbin -dCocoa @extrafpc.cfg corelib/cocoa/fpgui_toolkit.pas
+#$fpcbin -dCocoa @extrafpc.cfg corelib/cocoa/fpgui_toolkit.pas
