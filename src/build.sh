4f15a50fded751004a44784738fd1b49e018d493
Graeme Geldenhuys <graemeg@gmail.com>
2023-04-16 17:11:47 +0100

    Update build script to include AggCanvas options for Linux & FreeBSD.

--- src/build.sh
+++ src/build.sh
@@ -3,7 +3,9 @@
 echo "Which platform are you building for?"
 echo " "
 echo "  L - Linux"
+echo "  1 - Linux + AggCanvas"
 echo "  F - FreeBSD"
+echo "  2 - FreeBSD + AggCanvas"
 echo "  M - Mac OSX"
 echo " "
 
@@ -29,11 +31,19 @@ case $OSinput in
 		echo "Compiling for Linux"
 		$fpcbin -dX11 @extrafpc.cfg corelib/x11/fpgui_toolkit.pas
 		;;
+	"1")
+		echo "Compiling for Linux"
+		$fpcbin -dX11 -dAGGCanvas @extrafpc.cfg corelib/x11/fpgui_toolkit.pas
+		;;
 	"F"|"f")
 		echo "Compiling for FreeBSD"
 		$fpcbin -dX11 @extrafpc.cfg corelib/x11/fpgui_toolkit.pas
 		;;
-	"M"|"m")
+	"2")
+		echo "Compiling for FreeBSD + AggCanvas"
+		$fpcbin -dX11 -dAGGCanvas @extrafpc.cfg corelib/x11/fpgui_toolkit.pas
+		;;
+    "M"|"m")
 		echo "Compiling for OSX Cocoa"
 		$fpcbin -dCocoa @extrafpc.cfg corelib/cocoa/fpgui_toolkit.pas
 		;;
