8d4bc123d75cb269c1bcfbf2c3d2f9cd6af3c9be
Graeme Geldenhuys <graemeg@gmail.com>
2025-10-10 16:04:44 +0100

    improvement: (#123) translation .po files in 'languages' directory
    This implements a feature request to search for *.po files in a
    languages directory first, for cleaner project layout. It has a
    fallback to also search the base directory where the application is
    , for backwards compatibility.

--- src/corelib/fpg_translations.pas
+++ src/corelib/fpg_translations.pas
@@ -1,7 +1,7 @@
 {
     This unit is part of the fpGUI Toolkit project.
 
-    Copyright (c) 2006 - 2015 by Graeme Geldenhuys.
+    Copyright (c) 2006 - 2025 by Graeme Geldenhuys.
 
     See the file COPYING.modifiedLGPL, included in this distribution,
     for details about redistributing fpGUI.
@@ -65,7 +65,7 @@ uses
   ,fpg_pofiles
   ,fpg_utils
   ;
-  
+
 
 var
   TranslationList: TTranslationList;
@@ -73,11 +73,35 @@ var
   SystemLanguageID2: string = '';
 
 
-procedure CollectTranslations(const BaseAppName, BaseDir: string);
+procedure SearchForPOfiles(const BaseAppName, SearchDir: string);
 var
   FileInfo: TSearchRec;
   ID: string;
   SearchMask: string;
+begin
+  {$IFDEF GDEBUG}
+  writeln('Searching for PO files in ' + SearchDir);
+  {$ENDIF}
+  SearchMask := fpgAddTrailingValue(SearchDir, PathDelim, false) + BaseAppName + '.*.po';
+  if SysUtils.FindFirst(SearchMask, faAnyFile, FileInfo) = 0
+  then begin
+    repeat
+      if (FileInfo.Name = '.') or (FileInfo.Name = '..') or (FileInfo.Name = '') then
+        Continue;
+      ID := copy(FileInfo.Name,length(BaseAppName + '.')+1,
+               length(FileInfo.Name)-length(BaseAppName + '..po'));
+      if (ID <> '') and (Pos('.',ID) < 1) and (TranslationList.IndexOf(ID) < 0) then
+      begin
+        TranslationList.Add(ID);
+      end;
+    until SysUtils.FindNext(FileInfo) <> 0;
+  end;
+  SysUtils.FindClose(FileInfo);
+end;
+
+procedure CollectTranslations(const BaseAppName, BaseDir: string);
+var
+  LanguagesDir: string;
 begin
   // search for all <BaseAppName>.xxx.po files
   if TranslationList = nil then
@@ -87,26 +111,16 @@ begin
   // add automatic and english translation
   TranslationList.Add('');
   TranslationList.Add('en');
-  // search existing translations
 
-  SearchMask := fpgAddTrailingValue(BaseDir, PathDelim, false) + BaseAppName + '.*.po';
-  //writeln('CollectTranslations ',SearchMask);
-  if SysUtils.FindFirst(SearchMask, faAnyFile, FileInfo) = 0
-  then begin
-    repeat
-      if (FileInfo.Name = '.') or (FileInfo.Name = '..') or (FileInfo.Name = '') then
-        Continue;
-      ID := copy(FileInfo.Name,length(BaseAppName + '.')+1,
-               length(FileInfo.Name)-length(BaseAppName + '..po'));
-      //writeln('CollectTranslations A ',FileInfo.Name,' ID=',ID);
-      if (ID <> '') and (Pos('.',ID) < 1) and (TranslationList.IndexOf(ID) < 0) then
-      begin
-        //writeln('CollectTranslations ID=',ID);
-        TranslationList.Add(ID);
-      end;
-    until SysUtils.FindNext(FileInfo) <> 0;
-  end;
-  SysUtils.FindClose(FileInfo);
+  // Search in 'languages' sub-directory for cleaner project layouts.
+  LanguagesDir := fpgAddTrailingValue(BaseDir, PathDelim, false) + 'languages';
+  if fpgDirectoryExists(LanguagesDir) then
+    SearchForPOfiles(BaseAppName, LanguagesDir);
+
+  // If no translations were found in the 'languages' directory, then search
+  // in the application's root directory for backward compatibility.
+  if TranslationList.Count < 3 then // default has 2 ('', 'en')
+    SearchForPOfiles(BaseAppName, BaseDir);
 end;
 
 procedure TranslateResourceStrings(const BaseAppName, BaseDirectory, CustomLang: string);
@@ -116,6 +130,8 @@ var
   Lang: string;
   FallbackLang: string;
   Dir: string;
+  LangDir: string;
+  POFile: string;
 begin
   {$IFDEF GDEBUG}
   writeln('BaseAppName = ',BaseAppName);
@@ -124,7 +140,7 @@ begin
   {$ENDIF}
   if TranslationList = nil then
     CollectTranslations(BaseAppName, BaseDirectory);
-    
+
   if CustomLang = '' then
   begin
     Lang := SystemLanguageID1;
@@ -135,24 +151,26 @@ begin
     Lang := CustomLang;
     FallbackLang := '';
   end;
-//  writeln('TranslateResourceStrings A Lang=',Lang,' FallbackLang=',FallbackLang);
+
   Dir := fpgAddTrailingValue(BaseDirectory, PathDelim, false);
+  LangDir := Dir + 'languages' + PathDelim;
+
+  // First check for languages directory
+  if fpgDirectoryExists(LangDir) then
+    POFile := LangDir + BaseAppName + Ext
+  else
+    POFile := Dir + BaseAppName + Ext;
 
   {$IFDEF GDEBUG}
   writeln('Lang = ' + Lang);
   writeln('SystemLanguageID1 = ' + SystemLanguageID1);
   writeln('SystemLanguageID2 = ' + SystemLanguageID2);
-  writeln('Translation file = ' +Dir + BaseAppName + Ext);
+  writeln('Translation file = ' + POFile);
   {$ENDIF}
 
   // We use one translation file for all fpGUI Toolkit related text and one
   // translation file for all fpGUI based application text
-//  if BaseAppName = 'fpgui' then
-//    TranslateUnitResourceStrings('',
-//      Dir + BaseAppName + Ext, Lang, FallbackLang)
-//  else
-    TranslateUnitResourceStrings('fake',
-      Dir + BaseAppName + Ext, Lang, FallbackLang);
+  TranslateUnitResourceStrings('fake', POFile, Lang, FallbackLang);
 end;
 
 // Strip the '.' onwards part. eg: en_ZA.UTF-8  ->  en_ZA
@@ -231,4 +249,3 @@ finalization
   TranslationList := nil;
 
 end.
-
