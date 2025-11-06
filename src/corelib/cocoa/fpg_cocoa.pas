d955b341f813444e730abdbcccbe895c7ad99f34
Graeme Geldenhuys <graemeg@gmail.com>
2025-11-06 05:57:25 +0000

    fix: use NSString.stringWithUTF8String for proper string creation
    NSSTR macro is not available in FPC's CocoaAll.
    Use the correct CocoaAll API: NSString.stringWithUTF8String(PChar(...))
    
    This is the standard FPC way to create NSString from Pascal string.

--- src/corelib/cocoa/fpg_cocoa.pas
+++ src/corelib/cocoa/fpg_cocoa.pas
@@ -796,7 +796,7 @@ procedure TfpgCocoaWindow.DoSetWindowTitle(const ATitle: string);
 begin
   if not HandleIsValid then
     Exit;
-  FWinHandle.setTitle(NSSTR(PChar(ATitle)));
+  FWinHandle.setTitle(NSString.stringWithUTF8String(PChar(ATitle)));
 end;
 
 procedure TfpgCocoaWindow.DoSetMouseCursor;
@@ -1045,7 +1045,7 @@ begin
   pasteboard.clearContents;
 
   // Set the string
-  nsStr := NSSTR(PChar(AValue));
+  nsStr := NSString.stringWithUTF8String(PChar(AValue));
   types := NSArray.arrayWithObject(NSStringPboardType);
   pasteboard.declareTypes_owner(types, nil);
   pasteboard.setString_forType(nsStr, NSStringPboardType);
