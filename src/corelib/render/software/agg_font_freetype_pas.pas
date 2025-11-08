4af5aa53f8b755ed3955e4bbb3a2eec65d134ce9
Graeme Geldenhuys <graemeg@gmail.com>
2025-11-08 17:32:11 +0000

    debug: Output all points during TT_Outline to FT_Outline copy
    Add comprehensive debug output to FT_Load_Glyph showing:
    - Glyph index, n_Points, n_Contours
    - TT_Outline.points pointer address
    - ALL points being copied (not just points 5-10)
    - Both TT_Vector source and FT_Vector destination values for each point
    
    This will reveal whether the garbage coordinates at points 7-8
    (x=-602082 for point 7, x=-602017 for point 8) are:
    1. Already present in TT_Outline data from TrueType glyph loader
    2. Corrupted during the copy operation
    3. Result of array indexing or pointer arithmetic issue
    
    The pattern of valid Y coordinates but garbage X coordinates suggests
    a struct layout or stride problem rather than uninitialized memory.

--- src/corelib/render/software/agg_font_freetype_pas.pas
+++ src/corelib/render/software/agg_font_freetype_pas.pas
@@ -975,15 +975,18 @@ begin
   // TT_Outline.points is TT_Points = ^array of TT_Vector
   // Need to copy field-by-field because TT_Vector and FT_Vector are incompatible types
   SetLength(face^.glyph^.outline.points, outline.n_Points);
-  WriteLn('[FT_Load_Glyph] Copying ', outline.n_Points, ' points for glyph ', glyph_index);
+  WriteLn('[FT_Load_Glyph] Glyph ', glyph_index, ': n_Points=', outline.n_Points,
+          ' n_Contours=', outline.n_Contours);
+  WriteLn('[FT_Load_Glyph] Copying points from TT_Outline.points pointer: ',
+          PtrUInt(outline.points));
   for i := 0 to outline.n_Points - 1 do
   begin
     face^.glyph^.outline.points[i].x := outline.points^[i].x;
     face^.glyph^.outline.points[i].y := outline.points^[i].y;
-    if i >= 5 and i <= 10 then
-      WriteLn('[FT_Load_Glyph]   Point ', i, ': TT=(', outline.points^[i].x, ', ', outline.points^[i].y,
-              ') -> FT=(', face^.glyph^.outline.points[i].x, ', ', face^.glyph^.outline.points[i].y, ')');
+    WriteLn('[FT_Load_Glyph]   Point ', i, ': TT=(', outline.points^[i].x:8, ', ', outline.points^[i].y:8,
+            ') -> FT=(', face^.glyph^.outline.points[i].x:8, ', ', face^.glyph^.outline.points[i].y:8, ')');
   end;
+  WriteLn('[FT_Load_Glyph] Copy complete');
 
   // Allocate and copy tags (flags)
   // TT_Outline.flags is TT_PTouchTable = ^array of byte
