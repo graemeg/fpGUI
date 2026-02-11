14705252100597fb39ddb84dba47329ffa496223
Graeme Geldenhuys <graemeg@gmail.com>
2026-02-11 17:07:58 +0000

    fix: AggPas Win32 font engine compilation and rendering
    - Add {$IFDEF} splits for AGG2D_USE_WINFONTS vs AGG2D_USE_FREETYPE in
      Agg2D.pas (TfpgAgg2DFontResource and TAgg2D.Font)
    - Win32 path uses create_font_ with typeface names and GetTextMetrics
      for ascent/descent (Win32 engine lacks load_font/_ascender/_descender)
    - Fix DPI scaling for Win32 vector font cache (was using raw point size
      as pixel height, causing fonts to render too small)
    - DoSetFontRes passes FamilyName instead of FontPath on Win32
    - Make fpg_fontcache.pas conditionally compile FreeType-dependent code,
      removing the freetype.dll requirement when using Win32 font engine
    
    Fixes bug #144

--- framework/src/main/pascal/corelib/render/software/fpg_fontcache.pas
+++ framework/src/main/pascal/corelib/render/software/fpg_fontcache.pas
@@ -26,6 +26,18 @@ unit fpg_fontcache;
 
 {$mode objfpc}{$H+}
 
+{ Replicate font engine selection from agg_mode.inc (cannot include it
+  directly because it sets {$MODE delphi} which conflicts with objfpc). }
+{$IFDEF WINDOWS}
+  {$DEFINE AGG2D_USE_WINFONTS}
+{$ENDIF}
+{$if defined(UNIX) and not defined(DARWIN)}
+  {$DEFINE AGG2D_USE_FREETYPE}
+{$ENDIF}
+{$IFDEF DARWIN}
+  {$DEFINE AGG2D_USE_FREETYPE}
+{$ENDIF}
+
 interface
 
 uses
@@ -88,8 +100,11 @@ function gFontCache: TFontCacheList;
 implementation
 
 uses
-  fpg_utils,
-  agg_font_freetype_lib;
+  fpg_utils
+  {$IFDEF AGG2D_USE_FREETYPE}
+  , agg_font_freetype_lib
+  {$ENDIF}
+  ;
 
 const
   FPG_FONT_STYLE_REGULAR = 1 shl 0;     { Regular, Plain, Book }
@@ -104,7 +119,9 @@ const
   FPG_FONT_STYLE_FIXEDWIDTH = 1 shl 9;  { Fixedwidth }
 
 var
+  {$IFDEF AGG2D_USE_FREETYPE}
   m_library: FT_Library_ptr;
+  {$ENDIF}
   uFontCacheList: TFontCacheList;
 
 function gFontCache: TFontCacheList;
@@ -172,6 +189,7 @@ end;
 { TFontCacheList }
 
 procedure TFontCacheList.SearchForFont(const AFontPath: TfpgString);
+{$IFDEF AGG2D_USE_FREETYPE}
 var
   sr: TSearchRec;
   lFont: TFontCacheItem;
@@ -202,8 +220,14 @@ begin
   end;
   FindClose(sr);
 end;
+{$ELSE}
+begin
+  // No-op: Win32 font engine uses typeface names directly
+end;
+{$ENDIF}
 
 function TFontCacheList.BuildFontCacheItem(const AFontFile: TfpgString): TFontCacheItem;
+{$IFDEF AGG2D_USE_FREETYPE}
 var
   face_ptr: FT_Face_ptr;
   s: Ansistring;
@@ -236,6 +260,11 @@ begin
 
   FT_Done_Face(face_ptr);
 end;
+{$ELSE}
+begin
+  Result := TFontCacheItem.Create(AFontFile);
+end;
+{$ENDIF}
 
 procedure TFontCacheList.SetStyleIfExists(var AText: Ansistring; var AStyleFlags: integer;
   const AStyleName: AnsiString; const AStyleBit: integer);
@@ -278,6 +307,7 @@ begin
 end;
 
 procedure TFontCacheList.BuildFontCache;
+{$IFDEF AGG2D_USE_FREETYPE}
 var
   lPath: TfpgString;
   lPathList: TStringList;
@@ -314,6 +344,11 @@ begin
     lPathList.Free;
   end;
 end;
+{$ELSE}
+begin
+  // No-op: Win32 font engine uses typeface names directly, no font file cache needed
+end;
+{$ENDIF}
 
 function TFontCacheList.Add(const AObject: TFontCacheItem): integer;
 begin
