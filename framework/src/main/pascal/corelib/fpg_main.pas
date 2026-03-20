dc494364f976722397b9c478ce3ff0ad1cbc7a12
Graeme Geldenhuys <graemeg@gmail.com>
2026-03-20 18:55:19 +0000

    tab: improved pagecontrol tab rendering to be Font + DPI aware

--- framework/src/main/pascal/corelib/fpg_main.pas
+++ framework/src/main/pascal/corelib/fpg_main.pas
@@ -2766,8 +2766,12 @@ begin
 end;
 
 function TfpgStyle.GetDefaultTabHeight: TfpgCoord;
+var
+  lScreenDpi: integer;
 begin
-  Result := 21;
+  { Font height + DPI-scaled vertical padding (4px top + 4px bottom at 96 DPI) }
+  lScreenDpi := fpgApplication.Desktop.Screen_dpi(fpgApplication.Desktop.PrimaryScreen);
+  Result := GetTabFont.GetHeight + Round(8 * lScreenDpi / fpgApplication.DesignedDPI);
 end;
 
 procedure TfpgStyle.DrawTabBackground(ACanvas: TfpgCanvas; ABGColor: TfpgColor);
